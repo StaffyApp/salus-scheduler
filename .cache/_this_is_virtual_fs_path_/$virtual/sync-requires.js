05648a78e2816ea19b7b8624e12150bc4233213c
sumaiya897 <sumaiya@staffy.com>
2026-05-06 02:52:32 +0500

    add cross origin policy

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,6 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Admin\\workspace\\salus-scheduler\\.cache\\dev-404-page.js")),
   "component---src-pages-contact-js": preferDefault(require("C:\\Users\\Admin\\workspace\\salus-scheduler\\src\\pages\\contact.js")),
   "component---src-pages-faq-js": preferDefault(require("C:\\Users\\Admin\\workspace\\salus-scheduler\\src\\pages\\faq.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Users\\Admin\\workspace\\salus-scheduler\\src\\pages\\index.js"))
